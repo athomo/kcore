536ebd667577c133e0ccb18c5541d1d83d2dd1b6
unknown <Alex@DESKTOP-2E9HEQK.uvic.ca>
2017-08-15 09:35:53 -0700

    removing some files

--- src/Graph.java
+++
@@ -1,9 +0,0 @@
-import java.util.Iterator;
-
-public interface Graph {
-	public int maxDegree();
-	public int size();
-	public int[] getNeighbors(int u);
-	public int outdegree(int u);
-	public Iterator<Integer> vertexIterator();
-}
